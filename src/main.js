f450a47472283761e80dc27a8c7de2853ffd7483
CyriacBr <cyriacbr@gmail.com>
2019-11-22 10:03:26 +0100

    expected fail

--- src/main.js
+++ src/main.js
@@ -3,16 +3,24 @@ const app = express();
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const port = 3040;
-const { sleep } = require("./task");
+const { sleep, addition, multiplication } = require("./task");
 
 app.use(cors());
 app.use(bodyparser.json());
 
-app.post("/", (req, res) => {
+app.post("/addition", (req, res) => {
   const [nbr1, nbr2] = req.body.numbers;
   sleep(5000);
   res.json({
-    value: nbr1 + nbr2
+    value: addition(nbr1, nbr2)
+  });
+});
+
+app.post("/multiplication", (req, res) => {
+  const [nbr1, nbr2] = req.body.numbers;
+  sleep(5000);
+  res.json({
+    value: multiplication(nbr1, nbr2)
   });
 });
 
